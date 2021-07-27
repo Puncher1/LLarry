f0362ef265df7ee27e94743533b5fa04c683a394
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-07-27 22:36:46 +0200

    Added general

---
+++ utils/general.py
@@ -0,0 +1,56 @@
+import typing
+
+import discord
+from discord.ext import commands
+# end imports
+
+
+# Embed generator
+async def embed_gen(
+        channel: discord.abc.Messageable,
+        title: typing.Union[
+            str,
+            None],
+        description: typing.Union[
+            str,
+            None
+        ],
+        footer_text: typing.Union[
+            str,
+            None
+        ],
+        thumbnail_url: typing.Union[
+            str,
+            None
+        ],
+        image_url: typing.Union[
+            str,
+            None
+        ],
+        color: typing.Union[
+            int,
+            discord.Colour,
+            None
+        ],
+        return_embed: bool
+):
+    """A coroutine that returns a ``discord.Embed`` or a ``discord.Message``"""
+
+    Empty = discord.Embed.Empty
+
+    embed = discord.Embed()
+
+    embed.title = title if title else Empty
+    embed.description = description if description else Empty
+    embed.set_footer(text=footer_text if footer_text else Empty)
+    embed.set_thumbnail(url=thumbnail_url if footer_text else Empty)
+    embed.set_image(url=image_url if image_url else Empty)
+    embed.colour = color if color else Empty
+
+    if not return_embed:
+        await channel.trigger_typing()
+        message = await channel.send(embed=embed)
+        return message
+
+    else:
+        return embed
