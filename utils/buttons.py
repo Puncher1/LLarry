37d640fd53164701591b808a5117790e5d153779
Puncher1 <65789180+Puncher1@users.noreply.github.com>
2021-07-30 16:12:07 +0200

    Only author can use the menu

--- utils/buttons.py
+++ utils/buttons.py
@@ -7,13 +7,15 @@ from cogs.global_ import Global as g
 class HelpSelectMenu(discord.ui.Select):
     """A class that represents a function that adds a select menu to the help message"""
 
-    def __init__(self, client: commands.Bot, options: list):
+    def __init__(self, client: commands.Bot, options: list, user: discord.Member):
         self.client = client
         self.options_ = options
+        self.user = user
         super().__init__(placeholder="Choose a category", options=self.options_)
 
     async def callback(self, interaction: discord.Interaction):
-        self.view.value = interaction.data["values"][0]
-        self.view.stop()
+        if interaction.user == self.user:
+            self.view.value = interaction.data["values"][0]
+            self.view.stop()
 
 
